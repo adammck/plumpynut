d3b7447ba0fc1c1c1110d409a6318def4ad15d3c
Evan Wheeler <ewheeler@abacus.(none)>
2008-10-12 21:20:09 +0300

    removed indentation error

--- webui/inventory/models.py
+++ webui/inventory/models.py
@@ -63,7 +63,6 @@ class Report(models.Model):
 		return "%s report" % self.supply.name
 	
 	def _get_latest_entry(self):
-	"Returns latest entry of a report"
 		return Entry.objects.order_by('time)[0]
 
 	latest_entry = property(_get_latest_entry)
