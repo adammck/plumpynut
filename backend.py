599d6b77fa9182f4823e32b076746e869c1a4041
Adam Mckaig <adam.mckaig@gmail.com>
2008-10-12 12:58:21 -0400

    abstracted away __get_reporter

--- backend.py
+++ backend.py
@@ -20,42 +20,42 @@ from webui.inventory.models import *
 class App(SmsApplication):
 	kw = SmsKeywords()
 	
+	def __get_reporter(self, **kwargs):
+		try:
+			r = Reporter.objects.get(**kwargs)
+			return r
+		
+		except ObjectDoesNotExist:
+			return None
+	
+	
 	# IDENTIFY <ALIAS>
 	@kw("identify (letters)", "this is (letters)", "i am (letters)")
 	def identify(self, caller, alias):
 
 		# attempt to find the reporter by his/her alias,
 		# and notify them that they were recognized
-		try:
-			reporter = Reporter.objects.get(alias=alias)
-			self.send(caller, "Hello, %s" % reporter)
-			
-		except ObjectDoesNotExist:
+		reporter = self.__get_reporter(alias=alias)
+		if not reporter:
 			self.send(caller, "Sorry, I don't know anyone called %s" % (alias))
 			self.log("Unknown alias: %s" % (alias), "warn")
 			return
-		
-		
+
 		# if this reported is already associated
 		# with this number, there's nothing to do
 		if reporter.phone == caller:
+			self.send(caller, "Hello again, %s" % (reporter))
 			return
-		
-		
+
 		# if anyone else is currently identified
 		# by this number, then disassociate them
-		try:
-			prev = Reporter.objects.get(phone=caller)
-			if prev.pk != reporter.pk:
-				self.log("%s is no longer %s" % (prev, caller))
-				prev.phone = ""
-				prev.save()
-		
-		except ObjectDoesNotExist:
-			pass
-		
-		
+		prev = self.__get_reporter(phone=caller)
+		if prev and (prev.pk != reporter.pk):
+			prev.phone = ""
+			prev.save()
+
 		# associate the reporter with this number
+		self.send(caller, "Hello, %s" % (reporter))
 		reporter.phone = caller
 		reporter.save()
 	
@@ -64,29 +64,24 @@ class App(SmsApplication):
 	@kw("who is (letters)", "who (letters)", "(letters)\?")
 	def who(self, caller, alias):
 		
-		# attempt to find a reporter by that alias
+		# attempt to find a reporter by  alias
 		# and return their details to the caller
-		try:
-			reporter = Reporter.objects.get(alias=alias)
-			number = reporter.phone or "no number"
-			self.send(caller, "%s is %s [%s]" % (alias, reporter, number))
-			
-		except ObjectDoesNotExist:
-			self.send(caller, "Sorry, I don't know anyone called %s" % (alias))
+		reporter = self.__get_reporter(alias=alias)
+		if reporter: msg = "%s is %s" % (alias, reporter)
+		else:        msg = "I don't know anyone called %s" % (alias)
+		self.send(caller, msg)
 
 
 	# WHO AM I
 	@kw("who am i", "whoami")
 	def whoami(self, caller):
-		
+
 		# attempt to find a reporter matching the
 		# caller's phone number, and remind them
-		try:
-			reporter = Reporter.objects.get(phone=caller)
-			self.send(caller, "You are %s" % (reporter))
-		
-		except ObjectDoesNotExist:
-			self.send(caller, "Sorry, I don't know who you are")
+		reporter = self.__get_reporter(phone=caller)
+		if reporter: msg = "You are %s" % (reporter)
+		else:        msg = "I don't know who you are"
+		self.send(caller, msg)
 
 
 	# FLAG <NOTICE>
