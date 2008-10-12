208049a528e1991da8019efe7089c5f159347e1d
Adam Mckaig <adam.mckaig@gmail.com>
2008-10-12 14:52:15 -0400

    reporter phone# now optional (they might share), and stock levels accept null (unknown)

--- webui/inventory/models.py
+++ webui/inventory/models.py
@@ -7,7 +7,7 @@ class Reporter(models.Model):
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	alias = models.CharField(max_length=16, unique=True)
-	phone = models.CharField(max_length=30, unique=True)
+	phone = models.CharField(max_length=30, blank=True)
 	email = models.EmailField(blank=True)
 
 	def __unicode__(self):
@@ -35,7 +35,7 @@ class Location(models.Model):
 class SupplyLocation(models.Model):
 	supply = models.ForeignKey(Supply)
 	location = models.ForeignKey(Location)
-	quantity = models.IntegerField()
+	quantity = models.IntegerField(null=True, blank=True) # null=UNKNOWN
 	
 	def __unicode__(self):
 		return "%s at %s" %\
