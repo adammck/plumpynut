4884ee597993542be23636c2564c5d1ce046c9fd
Evan Wheeler <ewheeler@abacus.(none)>
2008-10-13 02:06:59 +0300

    added full_name property to reporter

--- webui/inventory/models.py
+++ webui/inventory/models.py
@@ -14,6 +14,11 @@ class Reporter(models.Model):
 		ph = self.phone or "unknown"
 		return "%s %s [%s]" %\
 			(self.first_name, self.last_name, ph)
+	
+	def _get_full_name(self):
+		return "%s %s" % (self.first_name, self.last_name)
+	
+	full_name = property(_get_full_name)
 
 class Supply(models.Model):
 	name = models.CharField(max_length=100)
@@ -24,7 +29,7 @@ class Supply(models.Model):
 	
 	class Meta:
 		verbose_name_plural="Supplies"
-
+	
 class Location(models.Model):
 	name = models.CharField(max_length=100)
 	code = models.CharField(max_length=4)
@@ -52,7 +57,7 @@ class Notification(models.Model):
 	
 	def __unicode__(self):
 		return "%s by %s" %\
-		(self.time, self.reporter)
+		(self.time.strftime("%d/%m/%y"), self.reporter)
 
 class Report(models.Model):
 	supply = models.ForeignKey(Supply)
