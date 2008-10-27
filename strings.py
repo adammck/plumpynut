cd3f0e9d9ccc86d31bb19b9a84fc91db6c14e30d
User <user@kelete.(none)>
2008-10-27 20:14:40 +0300

    revised some help text

--- strings.py
+++ strings.py
@@ -12,22 +12,24 @@ ENGLISH = {
 	
 	"ident":          "Hello, %s",
 	"ident_again":    "Hello again, %s",
-	"ident_help":     SORRY + "Please tell me who you are by replying: I AM <USERNAME>",	
+	"ident_help":     SORRY + "Please tell me who you are by replying: I AM YOUR-USERNAME",	
 	"whoami":         "You are %s",
-	"whoami_unknown": "I don't know who you are. Please tell me by replying: I AM <USERNAME>",
-	"whoami_help":    SORRY + "To find out who you are identified as, please reply: WHO AM I",
+	"whoami_unknown": "I don't know who you are. Please tell me by replying: I AM YOUR-USERNAME",
+	"whoami_help":    SORRY + "To find out who you are registered as, please reply: WHO AM I",
 	"whois":          "%s is %s",
-	"whois_help":     SORRY + "Please tell me who you are searching for, by replying: WHO IS <USERNAME>",
+	"whois_help":     SORRY + "Please tell me who you are searching for, by replying: WHO IS USERNAME",
 	"alert_ok":       "Thank you. Your alert was received and sent to UNICEF",
-	"alert_help":     SORRY + "Please tell me what you are alerting, by replying: ALERT <NOTICE>",
-	"cancel_ok":      "Your last report has been deleted",	
+	"alert_help":     SORRY + "Please tell me what you are alerting, by replying: ALERT YOUR-NOTICE",
+	"cancel_ok":      "Thanks, your last report has been cancelled",	
 	"cancel_none":    "You have not submitted any reports today. If you wish to change an older entry, please call " + SUPPORT,
 	"cancel_help":    SORRY + "You may delete your last report by replying: CANCEL",
 	"supplies_help":  SORRY + "To list all supply codes, please reply: SUPPLIES",
 	"locations_help": SORRY + "To list all location codes, please reply: LOCATIONS",
 	
-	"help_main":   "Help options: IDENTIFY, REPORT or ALERT",
-	"help_help":   SORRY + "Please reply: HELP IDENTIFY, HELP REPORT or HELP ALERT",
-	"help_report": "To make a report, please reply: <SUPPLY-CODE> <LOCATION-CODE> <BENEFICIERIES> <QUANTITY> <CONSUMPTION> <BALANCE>"
+	"help_main":   "uniSMS Help: reply with HELP REGISTER for registration, HELP REPORT for entry formatting, or HELP ALERT for help with alerting",
+	"help_help":   SORRY + "Please reply: HELP REGISTER, HELP REPORT or HELP ALERT",
+	"help_report": "To make a report reply with: SUPPLY-CODE LOCATION-CODE BENEFICIERIES QUANTITY CONSUMPTION BALANCE. Separate data with spaces and reports with commas.",
+	"help_reg":  "If your mobile number is not registered, please reply: I AM YOUR-USERNAME",
+	"help_alert":  "To alert UNICEF, reply with ALERT followed by your notice"
 }
 
