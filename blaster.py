37275c2d42a8f48dee8c38925dee62b1ff8485d1
User <user@kelete.(none)>
2008-10-27 15:57:09 +0300

    more comments for wacky code

--- blaster.py
+++ blaster.py
@@ -22,6 +22,8 @@ def blastTest():
 	return blast(numbers, message, 'alias')
 
 def blastOff():
+	# specific blast for kicking of uniSMS
+	# in Ethiopia
 	message = "Welcome to uniSMS! Your number is now registered to %(alias)s. Reply with 'help' for more information."
 	peeps = Monitor.objects.all()
 	numbers = []
@@ -31,11 +33,27 @@ def blastOff():
 
 
 def blast(numbers, message, field=None):
+	# mass SMS blaster to send a message to a
+	# list of numbers. Messages to uniSMS Monitors
+	# can be personalized by including either 
+	# %(alias)s or %(first_name)s or %(last_name)s or
+	# %(__unicode)s in the message and passing the
+	# optional field parameter of either alias or
+	# first_name or last_name or __unicode__
+	# TODO allow multiple personalized strings
+	
 	sending = 0 
 	sender = kannel.SmsSender("user", "password")
 	for n in numbers:
+		# passing a field implies that
+		# the message includes
+		# personalizeable strings
 		if field:
 			try:
+				# if a field is given, try to look up the
+				# the Monitor from the number and then
+				# substitute the monitor's attribute
+				# for the personalized string
 				m = Monitor.objects.get(phone=n)
 				if hasattr(m, field):
 					attribute = getattr(m,field)
@@ -50,6 +68,8 @@ def blast(numbers, message, field=None):
 			except ObjectDoesNotExist:
 				print "Oops. I coudn't find a monitor for %s" % (n)
 				pass
+		# if a field is not given, blast the
+		# message directly to all given numbers
 		else:
 			sender.send(n, message)
 			sending += 1
@@ -64,20 +84,24 @@ if __name__ == "__main__":
 		numbers = []	
 		message = False
 		field = None
+
 		# iterate all given arguments
 		# except for the first (./blaster.py)
 		for arg in sys.argv[1:]:
+
 			# strip brackets and commas from the argument
 			# (we expect a list of numbers as strings)
 			num = arg.strip('[,]')
 			if num.isdigit():
 				numbers.append(num)
+
 			# if there are no more numbers and message
 			# has not been set, assume this arg is
 			# the message
 			else:
 				if not message:
 					message = arg
+
 			# if we are still looping and have a
 			# message, try to assign optional
 			# field argument
@@ -86,6 +110,7 @@ if __name__ == "__main__":
 					field = arg
 			except:
 				field = None
+
 		# blast away	
 		blast(numbers, message, field)
 
